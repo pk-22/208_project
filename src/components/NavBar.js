8323def95b4479d612327ec7b0a71f588e0c6254
Prashant <prashantk2208@gmail.com>
2022-04-08 17:46:29 +0530

    added navbar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../navbar.css";
 class NavBar extends React.Component {
     render() {
         return (
-            <div>
-                <Link to="/Chat">Chat</Link>
-                <Link to="/">Home</Link>
-                <Link to="/Blog">Blog</Link>
-                <Link to="/Shop">Shop</Link>
-                <Link to="/Admin">Admin</Link>
+            <div className="nav">
+                <Link to="/Chat" className="links">Chat</Link>
+                <Link to="/" className="links">Home</Link>
+                <Link to="/Blog" className="links">Blog</Link>
+                <Link to="/Shop" className="links">Shop</Link>
+                <Link to="/Admin" className="links">Admin</Link>
             </div>
         );
     }
